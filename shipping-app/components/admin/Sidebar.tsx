00e2fb4128dcac481bc522681b0cf0f6f1d34592
tomas <tomas.totaro@gmail.com>
2026-05-27 18:56:15 -0300

    feature: colorear link actual en sidebar

--- shipping-app/components/admin/Sidebar.tsx
+++ shipping-app/components/admin/Sidebar.tsx
@@ -1,8 +1,14 @@
-import { Truck, LayoutDashboard, Package } from "lucide-react";
+"use client"; //porque agrego para que resalte el link en el que se encuentra
 
 import Link from "next/link";
 
+import { Truck, LayoutDashboard, Package } from "lucide-react";
+
+import { usePathname } from "next/navigation";
+
 export default function Sidebar() {
+  const pathname = usePathname();
+
   return (
     <div className="w-64 bg-slate-900 border-r border-slate-800 p-4">
       <h1 className="text-xl font-bold mb-6">AlquilAutos</h1>
@@ -11,7 +17,11 @@ export default function Sidebar() {
         {/* Dashboard */}
         <Link
           href="/dashboard"
-          className="flex items-center gap-2 p-2 rounded hover:bg-slate-800 transition"
+          className={`flex items-center gap-2 p-2 rounded transition ${
+            pathname === "/dashboard"
+              ? "bg-slate-800 text-white"
+              : "hover:bg-slate-800 text-slate-300"
+          }`}
         >
           <LayoutDashboard size={18} />
 
@@ -21,7 +31,11 @@ export default function Sidebar() {
         {/* Entregas */}
         <Link
           href="/entregas"
-          className="flex items-center gap-2 p-2 rounded hover:bg-slate-800 transition"
+          className={`flex items-center gap-2 p-2 rounded transition ${
+            pathname === "/entregas"
+              ? "bg-slate-800 text-white"
+              : "hover:bg-slate-800 text-slate-300"
+          }`}
         >
           <Truck size={18} />
 
@@ -31,7 +45,11 @@ export default function Sidebar() {
         {/* Coordinaciones */}
         <Link
           href="/coordinaciones"
-          className="flex items-center gap-2 p-2 rounded hover:bg-slate-800 transition"
+          className={`flex items-center gap-2 p-2 rounded transition ${
+            pathname === "/coordinaciones"
+              ? "bg-slate-800 text-white"
+              : "hover:bg-slate-800 text-slate-300"
+          }`}
         >
           <Package size={18} />
 
