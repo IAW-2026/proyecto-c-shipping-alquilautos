d20476ce3d9e9dfec05cefae961afb40b04aa8a5
tomas <tomas.totaro@gmail.com>
2026-06-25 15:21:17 -0300

    fix: contrato return endpoints para analytics app

--- shipping-app/app/api/metricas/distribucion-semanal/route.ts
+++ shipping-app/app/api/metricas/distribucion-semanal/route.ts
@@ -8,24 +8,33 @@ export async function GET() {
     const { userId, sessionClaims } = await auth();
 
     if (!userId) {
-      return Response.json({ error: "No autenticado" }, { status: 401 });
+      return Response.json(
+        { data: null, error: "No autenticado" },
+        { status: 401 },
+      );
     }
 
     //chequeo role
     const role = (sessionClaims?.publicMetadata as any)?.role;
 
     if (role !== "adminGlobal") {
-      return Response.json({ error: "No autorizado" }, { status: 403 });
+      return Response.json(
+        { data: null, error: "No autorizado" },
+        { status: 403 },
+      );
     }
 
     //obtengo los datos
     const data = await getDeliveryChartData();
-    return NextResponse.json(data);
+    return Response.json({
+      data,
+      error: null,
+    });
   } catch (error) {
     console.error(error);
 
     return Response.json(
-      { error: "Error obteniendo la distribución de estados" },
+      { data: null, error: "Error obteniendo la distribución de estados" },
       { status: 500 },
     );
   }
